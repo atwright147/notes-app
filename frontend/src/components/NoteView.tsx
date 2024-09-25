23f3057bce07d35885d3630b3a6033bc4feff746
Andy Wright <atwright147@gmail.com>
2024-09-25 18:07:12 +0100

    implement Milkdown Editor

--- frontend/src/components/NoteView.tsx
+++ frontend/src/components/NoteView.tsx
@@ -1,5 +1,54 @@
+import { Editor, rootCtx } from "@milkdown/kit/core";
+import { defaultValueCtx } from "@milkdown/kit/core";
+import { commonmark } from "@milkdown/kit/preset/commonmark";
+import { listener, listenerCtx } from "@milkdown/plugin-listener";
+import { Milkdown, MilkdownProvider, useEditor } from "@milkdown/react";
+import { nord } from "@milkdown/theme-nord";
+import { type FC, useMemo } from "react";
+
 import { useNoteQuery } from "@/hooks/useNoteQuery";
 import { useNotesStore } from "@/stores/notes.store";
+
+interface MilkdownEditorProps {
+	defaultValue: string;
+}
+
+const stripFrontmatter = (markdown: string | undefined): string => {
+	if (!markdown) return "";
+
+	const frontmatterRegex = /^---\s*[\s\S]*?\s*---\s*/;
+	return markdown.replace(frontmatterRegex, "");
+};
+
+const MilkdownEditor: FC = (): JSX.Element => {
+	const { selected } = useNotesStore();
+	const {
+		data: note,
+		isLoading: isNoteLoading,
+		isError: isNoteError,
+	} = useNoteQuery(selected ?? "");
+
+	const markdown = useMemo(() => stripFrontmatter(note), [note]);
+
+	useEditor(
+		(root) =>
+			Editor.make()
+				.config(nord)
+				.config((ctx) => {
+					ctx.set(defaultValueCtx, markdown);
+					ctx.set(rootCtx, root);
+					// ctx.set(defaultValueCtx, defaultValue);
+					ctx.get(listenerCtx).markdownUpdated((_, markdown) => {
+						console.log("Updated markdown:", markdown);
+					});
+				})
+				.use(commonmark)
+				.use(listener),
+		[markdown],
+	);
+
+	return <Milkdown />;
+};
 
 export const NoteView = () => {
 	const { selected } = useNotesStore();
@@ -10,10 +59,20 @@ export const NoteView = () => {
 		isError: isNoteError,
 	} = useNoteQuery(selected ?? "");
 
+	console.info("render");
+
+	const markdown = useMemo(() => stripFrontmatter(note), [note]);
+
 	return (
 		<div>
 			<h1>Note View</h1>
-			<pre>{JSON.stringify(note, null, 2)}</pre>
+			<pre>{JSON.stringify(markdown, null, 2)}</pre>
+
+			<hr />
+			<MilkdownProvider>
+				<MilkdownEditor />
+			</MilkdownProvider>
+			<hr />
 		</div>
 	);
 };
