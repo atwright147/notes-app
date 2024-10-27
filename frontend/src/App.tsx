5b6022d9fa303698b621703196c4f4134ee65365
Andy Wright <atwright147@gmail.com>
2024-10-27 21:44:29 +0000

    add action bar and move settings dialog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import "@/App.css";
 
 import { Grid, Provider, View, defaultTheme } from "@adobe/react-spectrum";
+import { ActionsBar } from "./components/ActionsBar";
 import { FileNav } from "./components/FileNav";
 import { MdEditor } from "./components/MdEditor";
 import { NoteView } from "./components/NoteView";
@@ -17,13 +18,18 @@ export const App = () => {
 		<Provider theme={defaultTheme} minHeight="100vh">
 			<Grid
 				UNSAFE_style={{ padding: "16px", boxSizing: "border-box" }}
-				areas={["sidebar content", "footer  footer"]}
-				columns={["250px", "1fr"]}
+				areas={["actionsbar sidebar content", "footer footer  footer"]}
+				columns={["40px", "250px", "1fr"]}
 				rows={["auto", "min-content"]}
 				minHeight="100vh"
 				gap="size-300"
 			>
+				<View gridArea="actionsbar">
+					<ActionsBar />
+				</View>
+
 				<View gridArea="content">
+					Content
 					<MdEditor />
 				</View>
 
---
+++ frontend/src/components/ActionsBar.tsx
@@ -0,0 +1,124 @@
+import {
+	Button,
+	ButtonGroup,
+	Content,
+	Dialog,
+	DialogContainer,
+	Divider,
+	Flex,
+	Heading,
+	TextField,
+} from "@adobe/react-spectrum";
+import DocumentIcon from "@spectrum-icons/workflow/Document";
+import FolderIcon from "@spectrum-icons/workflow/Folder";
+import SettingsIcon from "@spectrum-icons/workflow/Settings";
+import { type FC, useState } from "react";
+
+import { useMutation } from "@tanstack/react-query";
+import { OpenDirectoryDialog, SaveFullConfig } from "../../wailsjs/go/main/App";
+import { useConfigStoreQuery } from "../hooks/useConfigStoreQuery";
+import styles from "./ActionsBar.module.scss";
+
+interface FormValues {
+	notesDir: string;
+}
+
+export const ActionsBar: FC = (): JSX.Element => {
+	const [showSettingsDialog, setShowSettingsDialog] = useState(false);
+	const [notesDir, setNotesDir] = useState("");
+	const {
+		data: config,
+		isLoading: isConfigLoading,
+		isError: isConfigError,
+	} = useConfigStoreQuery();
+
+	const saveSettingsMutation = useMutation({
+		mutationFn: (values: FormValues): Promise<void> => {
+			return SaveFullConfig(values);
+		},
+		onSuccess: () => {
+			setShowSettingsDialog(false);
+		},
+		onError: () => console.info,
+	});
+
+	const handleSaveSettings = async (data: FormValues): Promise<void> => {
+		await saveSettingsMutation.mutate(data);
+	};
+
+	const handleOpenSettingsDialog = (): void => {
+		if (config?.notesDir) {
+			setNotesDir(config.notesDir);
+		}
+
+		setShowSettingsDialog(true);
+	};
+
+	const handleCloseSettingsDialog = (): void => {
+		setShowSettingsDialog(false);
+	};
+
+	const handleLocationDialog = async (): Promise<void> => {
+		const result = await OpenDirectoryDialog();
+		setNotesDir(result);
+	};
+
+	return (
+		<>
+			<Flex justifyContent="space-between" direction="column" height="100%">
+				<button type="button" className={styles.button}>
+					<DocumentIcon />
+				</button>
+				<button
+					type="button"
+					className={styles.button}
+					onClick={handleOpenSettingsDialog}
+				>
+					<SettingsIcon />
+				</button>
+			</Flex>
+
+			<DialogContainer type="modal" onDismiss={handleCloseSettingsDialog}>
+				{showSettingsDialog && (
+					<Dialog>
+						<Heading>Settings</Heading>
+						<Divider />
+						<ButtonGroup>
+							<Button variant="secondary" onPress={handleCloseSettingsDialog}>
+								Cancel
+							</Button>
+							<Button
+								autoFocus
+								variant="accent"
+								onPress={() => handleSaveSettings({ notesDir })}
+							>
+								Save
+							</Button>
+						</ButtonGroup>
+
+						<Content>
+							<Flex gap="size-100" direction="row" alignItems="end">
+								<TextField
+									label="Notes Folder"
+									name="notesDir"
+									value={notesDir}
+									onChange={setNotesDir}
+									isRequired
+									width="100%"
+								/>
+								<Button
+									type="button"
+									variant="secondary"
+									onPress={handleLocationDialog}
+									aria-label="Select a folder"
+								>
+									<FolderIcon />
+								</Button>
+							</Flex>
+						</Content>
+					</Dialog>
+				)}
+			</DialogContainer>
+		</>
+	);
+};
